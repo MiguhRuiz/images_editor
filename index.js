f46deb3aa18fa02cd98a3bfd001a82c9b4457385
Miguh Ruiz <miguhruiz@gmail.com>
2017-08-14 17:09:38 +0200

    [Init] Intiial Setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            devTools: true
+        }
+    })
+    win.loadURL(`file://${__dirname}/build/index.html`)
+})
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/build',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /(\.js|.jsx)$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
+
+module.exports = config
